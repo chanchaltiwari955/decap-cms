7f98b498abd351261b8fca928233f09520a6a212
chanchaltiwari955 <97329721+chanchaltiwari955@users.noreply.github.com>
2026-04-01 12:30:28 +0530

    Update next.config.js
    Configure Next.js to allow images from external sources.

--- next.config.js
+++ next.config.js
@@ -1,1 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // If your content includes images from external sources
+  images: {
+    domains: ['raw.githubusercontent.com'], 
+  },
+};
 
+module.exports = nextConfig;
